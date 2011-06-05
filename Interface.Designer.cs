7df49612ae2d8edfeb26584d9d5b486208de0b1d
pejump1 <pejump1@googlemail.com>
2011-06-05 19:56:27 +0000

    Updated the GUI slightly..

--- Interface.Designer.cs
+++ Interface.Designer.cs
@@ -29,26 +29,27 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logoBox = new System.Windows.Forms.PictureBox();
             this.logBox = new System.Windows.Forms.TextBox();
             this.informationButton = new System.Windows.Forms.Button();
             this.hotelalertButton = new System.Windows.Forms.Button();
             this.startButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // logoBox
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::JabboServer.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(113, 44);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.logoBox.BackColor = System.Drawing.Color.Transparent;
+            this.logoBox.Image = global::JabboServer.Properties.Resources.logo;
+            this.logoBox.Location = new System.Drawing.Point(12, 12);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(113, 44);
+            this.logoBox.TabIndex = 0;
+            this.logoBox.TabStop = false;
             // 
             // logBox
             // 
+            this.logBox.BackColor = System.Drawing.SystemColors.Control;
             this.logBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.logBox.Enabled = false;
             this.logBox.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -58,6 +59,8 @@
             this.logBox.ReadOnly = true;
             this.logBox.Size = new System.Drawing.Size(350, 252);
             this.logBox.TabIndex = 5;
+            this.logBox.MouseLeave += new System.EventHandler(this.logBox_MouseLeave);
+            this.logBox.MouseHover += new System.EventHandler(this.logBox_MouseHover);
             // 
             // informationButton
             // 
@@ -96,7 +99,7 @@
             this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.startButton.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Font = new System.Drawing.Font("Verdana", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startButton.ForeColor = System.Drawing.Color.White;
             this.startButton.Location = new System.Drawing.Point(12, 90);
             this.startButton.Name = "startButton";
@@ -116,11 +119,12 @@
             this.BackgroundImage = global::JabboServer.Properties.Resources.panel;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(650, 354);
+            this.ControlBox = false;
             this.Controls.Add(this.hotelalertButton);
             this.Controls.Add(this.informationButton);
             this.Controls.Add(this.logBox);
             this.Controls.Add(this.startButton);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.logoBox);
             this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -130,7 +134,7 @@
             this.Opacity = 0.9D;
             this.Text = "JabboServer [C#] - Users online: 0 - Rooms loaded: 0";
             this.Load += new System.EventHandler(this.Interface_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,7 +142,7 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox logoBox;
         private System.Windows.Forms.TextBox logBox;
         private System.Windows.Forms.Button informationButton;
         private System.Windows.Forms.Button hotelalertButton;
